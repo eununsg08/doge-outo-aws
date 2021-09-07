dda0de4d785206d794d80667c5e76be11a4fd1ac
eununsg08 <69507865+eununsg08@users.noreply.github.com>
2021-09-07 19:03:55 +0900

    Update OutoTrade.py

--- OutoTrade.py
+++ OutoTrade.py
@@ -50,7 +50,7 @@ while True:
         end_time = start_time + datetime.timedelta(days=1)
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-DOGE", 0.3)
+            target_price = get_target_price("KRW-DOGE", 0.4)
             ma15 = get_ma15("KRW-DOGE")
             current_price = get_current_price("KRW-DOGE")
             if target_price < current_price and ma15 < current_price:
